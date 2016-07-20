06c22f7bbac2330253941ff08efc5ee0c9402b8d
andrew brown <aebrownz11@gmail.com>
2016-07-19 17:59:34 -0700

    resolved conflicts

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const UserRouter = require('../users/users.routes');
 const ShopRouter = require('../shops/shops.routes');
+const MetricRouter = require('../metrics/metrics.routes');
 
 module.exports = (app) => {
   app.use('/v1/users', UserRouter);
   app.use('/v1/shops', ShopRouter);
+  app.use('/v1/metrics', MetricRouter);
 };
