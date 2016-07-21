9f96ec73c7270ca74c806c1f94263f8164b891de
Conway Wang <conway@Conways-MacBook-Air.local>
2016-07-21 15:01:44 -0700

    add cron job to delete metrics daily and get rating for an array of google pace IDs

--- metrics/metrics.controller.js
+++ metrics/metrics.controller.js
@@ -1,4 +1,5 @@
 const Metric = require('./metrics.model');
+var cron = require('node-cron');
 
 module.exports = {
   addRating
@@ -13,9 +14,32 @@ function addRating(req, res) {
     userMessage: req.body.message,
     userID: req.body.userId,
     shopID: req.body.shopId,
+    placeID: req.body.placeID,
     createdAt: new Date(),
     updatedAt: new Date()
   })
   .then(() => { res.status(200).end(); })
   .catch((err) => { console.log(err); });
 }
+
+function getRating(req, res) {
+    var ratings = {};
+    req.query.shops.forEach(id => {
+      ratings.shop.id = {rating: 0 count: 0};
+      Metric.findAll({where: {placeID: id}})
+      .then(function(posts) {
+        var end = new Date();
+        if(end.getTime() - posts.createdAt.getTime() < 7200000) { //if time in ms from 1-1-1970 is less than two hours then data is fresh enough
+          ratings.shop.id.rating += posts.availRating;
+          ratings.shop.id.count++;
+        }
+      });
+    })
+  res.send(ratings);
+}
+//on the cient side if count is 0 then there are no ratings else divide rating with count for average
+
+cron.schedule('00 30 3 * * *', function(){ //run cron job at 330 am every day
+  Metric.destroy({where: {}});
+});
+
