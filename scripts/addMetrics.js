8983f266c01ba3d000033ee4892d841ced988e3f
conway <conwaywang@gmail.com>
2016-07-24 18:08:00 -0700

    get and post metrics working and poplate database with dumy data

---
+++ scripts/addMetrics.js
@@ -0,0 +1,34 @@
+const Metric = require('../metrics/metrics.model');
+
+Metric.create({
+    availRating: 1,
+    userMessage: "cool place to be",
+    userID: 1,
+    placeID: "ChIJwcYC_Z59j4ARrSKjYjV6_1M"
+  })
+
+Metric.create({
+    availRating: 1,
+    userMessage: "test",
+    userID: 1,
+    placeID: "ChIJwcYC_Z59j4ARrSKjYjV6_1M"
+  })
+
+Metric.create({
+    availRating: 4,
+    userMessage: "test",
+    userID: 1,
+    placeID: "ChIJwcYC_Z59j4ARrSKjYjV6_1M"
+  })
+Metric.create({
+    availRating: 4,
+    userMessage: "test",
+    userID: 1,
+    placeID: "ChIJee4DupqHhYARXadb2DlBtr8"
+  })
+Metric.create({
+    availRating: 1,
+    userMessage: "test",
+    userID: 1,
+    placeID: "ChIJkd6w5n6HhYARSMFG6z99F1o"
+  })
