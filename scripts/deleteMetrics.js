c05f855bdf05a31f42d150b2be90485d4e32596a
conway <conwaywang@gmail.com>
2016-07-24 18:08:00 -0700

    get and post metrics working and poplate database with dumy data

--- scripts/deleteMetrics.js
+++ scripts/deleteMetrics.js
@@ -1,4 +1,4 @@
-const Metric = require('./metrics.model');
+const Metric = require('../metrics/metrics.model');
 var cron = require('node-cron');
 
 cron.schedule('00 30 3 * * *', function(){ //run cron job at 330 am every day
